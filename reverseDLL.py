53dee7ba94baaf5972d9df536b0982bf02c294ae
Boom-Ba <zzhu158@gwu.edu>
2020-08-27 12:29:27 -0400

    commit reverseDLL.py file

---
+++ reverseDLL.py
@@ -0,0 +1,171 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the sortedInsert function below.
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+def sortedInsert(head, data):
+    newN = DoublyLinkedListNode(data)
+    if not head:
+        return newN
+    elif newN.data< head.data:
+        newN.next= head
+        head.prev=newN
+        return newN
+    else:
+        rest =sortedInsert(head.next, data)
+        head.next= rest
+        rest.prev = head
+        return head       
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the reverse function below.
+
+#
+# For your reference:
+#  null<-1 <-> 2 2->1->null
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#curr curr ->prev
+def reverse(head):
+    
+    curr = head
+    newHead =head
+    while curr:
+        prev= curr.prev
+        curr.prev = curr.next
+        curr.next= prev
+        newHead = curr
+        curr= curr.prev
+
+    return newHead
+       # null <- 4 <-> 1 <-> 2  <->  3 -> null
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
